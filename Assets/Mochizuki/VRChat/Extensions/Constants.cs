17bf482aef9d9ba1e5f61d30b756f0cf2a8609f2
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-15 13:52:54 +0900

    chore: upgrade to 0.3.0

--- Assets/Mochizuki/VRChat/Extensions/Constants.cs
+++ Assets/Mochizuki/VRChat/Extensions/Constants.cs
@@ -7,6 +7,6 @@ namespace Mochizuki.VRChat.Extensions
 {
     public static class Constants
     {
-        public static string Version => "VRCSDK3A-0.2.1";
+        public static string Version => "VRCSDK3A-0.3.0";
     }
 }
