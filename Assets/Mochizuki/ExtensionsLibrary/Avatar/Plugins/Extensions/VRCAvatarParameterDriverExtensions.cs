85db8eae7ba55daa96205c2800a42f37b6563631
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-04 18:23:17 +0900

    refactor: change namespace

---
+++ Assets/Mochizuki/ExtensionsLibrary/Avatar/Plugins/Extensions/VRCAvatarParameterDriverExtensions.cs
@@ -0,0 +1,25 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using System.Linq;
+
+using VRC.SDK3.Avatars.Components;
+using VRC.SDKBase;
+
+namespace Mochizuki.ExtensionsLibrary.Avatar.Extensions
+{
+    // ReSharper disable once InconsistentNaming
+    public static class VRCAvatarParameterDriverExtensions
+    {
+        public static void CloneTo(this VRCAvatarParameterDriver source, VRCAvatarParameterDriver dest)
+        {
+            source.ApplySettings = dest.ApplySettings;
+            source.parameters = dest.parameters.Select(w => new VRC_AvatarParameterDriver.Parameter { name = w.name, value = w.value }).ToList();
+            source.debugString = dest.debugString;
+            source.name = dest.name;
+            source.hideFlags = dest.hideFlags;
+        }
+    }
+}
---
+++ Assets/Mochizuki/ExtensionsLibrary/Runtime/Plugins/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using System.Collections.Generic;
+
+namespace Mochizuki.ExtensionsLibrary.Runtime.Extensions
+{
+    internal static class DictionaryExtensions
+    {
+        public static TValue GetValueSafe<TKey, TValue>(this Dictionary<TKey, TValue> obj, TKey key)
+        {
+            return obj.GetValueSafeWithDefault(key, default);
+        }
+
+        public static TValue GetValueSafeWithDefault<TKey, TValue>(this Dictionary<TKey, TValue> obj, TKey key, TValue @default)
+        {
+            if (obj.ContainsKey(key))
+                return obj[key];
+            return @default;
+        }
+    }
+}
